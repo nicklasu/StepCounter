0e44b00ecd36d90d00556a6b97ca852460193e33
nicklasu <nicklass84@gmail.com>
2021-04-21 00:14:01 +0300

    Add Step to activity_main.xml, Add basic functionality to step counter. Unsure if works correctly. Needs debugging.

--- app/src/main/java/com/example/stepcounter/MainActivity.java
+++ app/src/main/java/com/example/stepcounter/MainActivity.java
@@ -2,14 +2,53 @@ package com.example.stepcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    int i = 0;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    //Get the SensorManager and attach it a name "sensorManager"
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    private boolean running = false;
+    private float totalSteps = 0f;
+    private TextView stepsTaken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stepsTaken = findViewById(R.id.stepsTaken);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+        running = true;
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) == null) {
+            Log.d("STEPCOUNTERDEBUG", "No sensor detected!");
+        } else {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+
+    }
+    public void onAccuracyChanged (Sensor sensor, int accuracy){
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if(running){
+            totalSteps = event.values[0];
+            stepsTaken.setText(String.valueOf(totalSteps));
+            Log.d("STEPCOUNTERDEBUG","Steps go up!");
+        }
+    }
 }
