686d60eaf5108a8fb00a4cd46f4ae77797d29276
nicklasu <nicklass84@gmail.com>
2021-04-22 14:22:14 +0300

    Add permission ask to onCreate. Earlier it didn't work partly because of it. Add integer transition to onSensorChanged, might be reworked later. Moved stepSensor initialization to onResume, where it should have been from the beginning.

--- app/src/main/java/com/example/stepcounter/MainActivity.java
+++ app/src/main/java/com/example/stepcounter/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.stepcounter;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -14,23 +20,44 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     //Get the SensorManager and attach it a name "sensorManager"
     private SensorManager sensorManager;
     private Sensor stepSensor;
+
     private float totalSteps = 0f;
+
     private TextView stepsTaken;
+
+
+    //For backwards compatibility to android version 8.0
+    @RequiresApi(api = Build.VERSION_CODES.Q)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check for permission for step counter
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        //Initialize sensorManager so it can getDefaultSensor later
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+
         stepsTaken = findViewById(R.id.stepsTaken);
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
 
     }
 
+
     @Override
     protected void onResume(){
         super.onResume();
+
+        //Uses sensorManager to get TYPE_STEP_COUNTER, which uses accelerometer to calculate steps
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        //Print debug log if sensor isn't detected. Else start registering steps
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) == null) {
             Log.d("STEPCOUNTERDEBUG", "No sensor detected!");
         } else {
@@ -38,15 +65,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
     }
+    //Has to be called, else SensorEventListener (implementation at the beginning) wouldn't work
     public void onAccuracyChanged (Sensor sensor, int accuracy){
 
     }
 
     public void onSensorChanged(SensorEvent event) {
 
+            //event.values has something to do with how accelerometer works.
+            //It is just how type_step_counter counts steps, using accelerometer.
             totalSteps = event.values[0];
-            String currentSteps = String.valueOf(totalSteps);
-            stepsTaken.setText(currentSteps);
+
+            //Put the values to int from float and round them
+            int totalStepsInt = Math.round(totalSteps);
+            stepsTaken.setText(String.valueOf(totalStepsInt));
             Log.d("STEPCOUNTERDEBUG","Steps go up!");
 
     }
