7a7974d54168dda8fd2736cd2857f0bd29d541ac
nicklasu <nicklass84@gmail.com>
2021-05-01 19:44:03 +0300

    Cleanup

--- app/src/main/java/com/example/stepcounter/dayDataSingleton.java
+++ app/src/main/java/com/example/stepcounter/dayDataSingleton.java
@@ -1,6 +1,11 @@
 package com.example.stepcounter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import java.util.ArrayList;
+
+import static com.example.stepcounter.MainActivity.STEP_COUNT_PREFERENCES;
 
 public class dayDataSingleton {
 
@@ -11,10 +16,15 @@ public class dayDataSingleton {
         return dayDataInstance;
     }
 
+    public void addValue(String date, int steps, double distance, int calories){
+        dayDatas.add(new dayData(date, steps, distance, calories));
+    }
+
     private dayDataSingleton(){
+
         dayDatas = new ArrayList<>();
         /* Test data 1.week */
-        dayDatas.add(new dayData("1.1.2021", 7124, 5.43, 200));
+        dayDatas.add(new dayData("1.1.2021", 2345, 5.43, 200));
         dayDatas.add(new dayData("2.1.2021", 8423, 6.42, 200));
         dayDatas.add(new dayData("3.1.2021", 5843, 4.45, 200));
         dayDatas.add(new dayData("4.1.2021", 4221, 3.22, 200));
