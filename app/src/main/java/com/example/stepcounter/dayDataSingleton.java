3d9ea23f62dc657e6ec6aa800ae717f1997b1c6e
Roboto45 <Tatu.Talvikko@outlook.com>
2021-05-03 01:50:35 +0300

    added methods to dayDataSingleton, save now works, load partly(it creates some duplicates) removed CalendarActivity.java bc not needed

--- app/src/main/java/com/example/stepcounter/dayDataSingleton.java
+++ app/src/main/java/com/example/stepcounter/dayDataSingleton.java
@@ -24,17 +24,34 @@ public class dayDataSingleton {
         dayDatas.add(new dayData(date, steps, distance, calories));
     }
 
+    public int getSize(){
+        return dayDatas.size();
+    }
+
+    public void del(int index){
+        dayDatas.remove(index);
+    }
+
+    public ArrayList<String> getAllDates(){
+        ArrayList<String> allDates = new ArrayList<>();
+        for (int i = 0; i < getSize(); i++){
+            allDates.add(dayDatas.get(i).getDate());
+        }
+        return allDates;
+    }
+
     private dayDataSingleton(){
 
         dayDatas = new ArrayList<>();
-        /* Test data 1.week */
-        dayDatas.add(new dayData("1.1.2021", 2345, 5.43, 200));
-        dayDatas.add(new dayData("2.1.2021", 8423, 6.42, 200));
-        dayDatas.add(new dayData("3.1.2021", 5843, 4.45, 200));
-        dayDatas.add(new dayData("4.1.2021", 4221, 3.22, 200));
-        dayDatas.add(new dayData("5.1.2021", 7432, 5.66, 200));
-        dayDatas.add(new dayData("6.1.2021", 4325, 3.3, 200));
-        dayDatas.add(new dayData("7.1.2021", 9523, 7.26, 200));
+        /* Test data 1.week *
+        dayDatas.add(new dayData("01/01/2021", 2345, 5.43, 200));
+        dayDatas.add(new dayData("02/01/2021", 8423, 6.42, 200));
+        dayDatas.add(new dayData("03/01/2021", 5843, 4.45, 200));
+        dayDatas.add(new dayData("04/01/2021", 4221, 3.22, 200));
+        dayDatas.add(new dayData("05/01/2021", 7432, 5.66, 200));
+        dayDatas.add(new dayData("06/01/2021", 4325, 3.3, 200));
+        dayDatas.add(new dayData("07/01/2021", 9523, 7.26, 200));
+        */
     }
 
 
